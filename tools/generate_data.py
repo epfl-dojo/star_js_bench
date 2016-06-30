86fbb77cb449d496861a3f23e02861879a66601f
Samuel Bancal <Samuel.Bancal@epfl.ch>
2016-06-30 17:25:21 +0200

    2016-06-30 pick colors and images recursively

--- tools/generate_data.py
+++ tools/generate_data.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python3
 
-# Prepare some data to be rendered
-
+"""
+Prepare summary.json for the bench of different JS frameworks.
+"""
 
 import os
-import sys
 import json
-import random
 
 PROJDIR = os.path.dirname(os.path.dirname(__file__))
-all_entries = []
+COLORS = ("Lavender", "Thistle", "Plum", "Violet",
+          "Orchid", "Fuchsia", "MediumOrchid", "MediumPurple",
+          "BlueViolet", "DarkViolet", "DarkOrchid", "DarkMagenta",
+          "Purple", "Indigo", "SlateBlue", "DarkSlateBlue",
+          "MediumSlateBlue")
+IMAGES = os.listdir(os.path.join(PROJDIR, "data/images/100_100"))
+IMAGES.sort()
+
 
 def all_files(path="/usr/lib"):
-    images = os.listdir(os.path.join(PROJDIR, "data/images/100_100"))
+    """
+    yield each entry with it's attributes
+    """
+    image_index = 0
+    color_index = 0
     for root, dirs, files in os.walk(path):
-        for f in files:
-            full_path = os.path.join(root, f)
+        for fil in files:
+            full_path = os.path.join(root, fil)
             try:
                 stat = os.stat(full_path)
-            except:
+            except FileNotFoundError:
                 next
             yield {
                 "filename":full_path,
@@ -26,13 +36,20 @@ def all_files(path="/usr/lib"):
                 "uid":stat.st_uid,
                 "gid":stat.st_gid,
                 "date":stat.st_ctime,
-                "color":random.choice(["Lavender", "Thistle", "Plum", "Violet",
-                  "Orchid", "Fuchsia", "MediumOrchid", "MediumPurple",
-                  "BlueViolet", "DarkViolet", "DarkOrchid", "DarkMagenta",
-                  "Purple", "Indigo", "SlateBlue", "DarkSlateBlue",
-                  "MediumSlateBlue"]),
-                "img":random.choice(images),
+                "color":COLORS[color_index],
+                "img":IMAGES[image_index],
             }
+            color_index = (color_index + 1) % len(COLORS)
+            image_index = (image_index + 1) % len(IMAGES)
 
-with open(os.path.join(PROJDIR, "data/summary.json"), "w") as f:
-    f.write(json.dumps(list(all_files()), indent=1))
+
+def main():
+    """
+    main
+    """
+    with open(os.path.join(PROJDIR, "data/summary.json"), "w") as fil:
+        fil.write(json.dumps(list(all_files()), indent=1))
+
+
+if __name__ == "__main__":
+    main()
